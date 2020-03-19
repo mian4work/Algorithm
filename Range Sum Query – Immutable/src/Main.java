02dedfb1bffbd5b345efeb871b38c962d73e7739
Mian Zhang <mian4work@yahoo.com>
2020-03-19 09:42:47 -0700

    Add preprocessed array

--- Range Sum Query – Immutable/src/Main.java
+++ Range Sum Query – Immutable/src/Main.java
@@ -8,5 +8,10 @@ public class Main {
 	    System.out.println(solution.sumRange(nums, 0, 2));
 		System.out.println(solution.sumRange(nums, 2, 5));
 		System.out.println(solution.sumRange(nums, 0, 5));
+
+		solution = new Solution(nums);
+		System.out.println(solution.sumRange(0, 2));
+		System.out.println(solution.sumRange(2, 5));
+		System.out.println(solution.sumRange(0, 5));
     }
 }
--- Range Sum Query – Immutable/src/Solution.java
+++ Range Sum Query – Immutable/src/Solution.java
@@ -14,6 +14,39 @@
     There are many calls to sumRange function.
  */
 public class Solution {
+    int[] input;
+
+    public Solution() {}
+
+    /**
+     * This problem is not as simple as it shows. The 'There are many calls to sumRange function.' means the function
+     * could be called many times. In this case, each call will talk O(n) time to process.
+     *
+     * So a pre process of the array is important to shorten the query time of function sumRange.
+     * Create another array to hold the pre processed data in array input.
+     *
+     * The pre process is like this:
+     *      Each index in array input will hold the sum from 0 to i, assuming input[0] == sum[0].
+     *      When we want to retrieve the sum between index i to j:
+     *          If i == 0: sum == input[j]
+     *          If i > 0 <= j: sum == input[j] - input[i]
+     *
+     *          Example:    nums:   [1, 2, 3, 4, 5]
+     *                      inputs: [1, 3, 6, 10, 15]
+     *
+     *         Result: i=0, j=3 -> sum = 10
+     *                 i=1, j=3 -> sum = 7
+     *
+     * @param nums
+     */
+    public Solution(int[] nums) {
+        input = new int[nums.length];
+        input[0] = nums[0];
+
+        for(int i = 1; i <= nums.length - 1; i++) {
+            input[i] = input[i - 1] + nums[i];
+        }
+    }
 
     /**
      * Brute force solution: loop through i to j and sum
@@ -33,5 +66,20 @@ public class Solution {
         }
 
         return sum;
+    }
+
+    /**
+     * Use pre processed array input.
+     *
+     * @param i
+     * @param j
+     * @return
+     */
+    public int sumRange(int i, int j) {
+        if(i == 0) {
+            return input[j];
+        } else {
+            return input[j] - input[i - 1]; //Important!!! should be i - 1 because i is included!!!
+        }
     }
 }
