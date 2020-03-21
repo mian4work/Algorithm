48218cf1525a9835df7433e15f4c0b55dced598d
Mian Zhang <mian4work@yahoo.com>
2020-03-21 09:38:46 -0700

    add more climbing ways

--- Climbing Stairs/src/Solution.java
+++ Climbing Stairs/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 /*
    You are climbing a stair case. It takes n steps to reach to the top.
 
@@ -22,6 +24,10 @@
         2. 1 step + 2 steps
         3. 2 steps + 1 step
  */
+/*
+    What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X?
+    For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+ */
 public class Solution {
 
     /**
@@ -39,5 +45,19 @@ public class Solution {
         }
 
         return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    /**
+     * Given a set of climb ways, do it one by one.
+     * @param n
+     * @param x
+     * @return
+     */
+    public int climbStairs(int n, Set<Integer> x) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        
+        return x.stream().reduce((a, b) -> climbStairs(n - a, x) + climbStairs(n - b, x)).get();
     }
 }
