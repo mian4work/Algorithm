d8c82332fda0c391e201ab1e5f45bb3f911e4bb0
Mian Zhang <mian4work@yahoo.com>
2020-03-22 15:41:51 -0700

    House Robber

---
+++ House Robber/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution solution = new Solution();
+	    int[] nums1 = new int[]{1,2,3,1};
+		int[] nums2 = new int[]{2,7,9,3,};
+		int[] nums3 = new int[]{2,1,1,2};
+	    //System.out.println(solution.rob(nums1));
+		//System.out.println(solution.rob(nums2));
+		System.out.println(solution.rob(nums3));
+    }
+}
