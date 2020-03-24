1ae51a92891f6d785fe0944a0198c87239daf9bd
Mian Zhang <mian4work@yahoo.com>
2020-03-24 11:15:40 -0700

    add some comparisons

--- Random Number from Stream/src/Main.java
+++ Random Number from Stream/src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 	    Solution solution = new Solution();
 
+	    int[] stream = new int[]{2, 5, 7, 3, 5, 7, 1, 9, 2, 4, 0, 1, 2, 4, 5};
 	    for(int i = 0; i < 100; i++) {
-            System.out.println(solution.pick(new int[]{2, 5, 7, 3, 5, 7, 1, 9, 2, 4, 0, 1, 2, 4, 5}));
+            System.out.println("The selected element with solution is " + solution.pick(stream) + " in comparison with " + stream[new Random().nextInt(stream.length)]);
         }
     }
 }
