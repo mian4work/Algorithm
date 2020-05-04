df89c22236bcf4801962becd0bc9cdf0e43b4dd9
Mian Zhang <mian4work@yahoo.com>
2020-05-04 10:50:41 -0700

    Number Complement

---
+++ Number Complement/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution solution = new Solution();
+	    int ans = solution.findComplement(Integer.MIN_VALUE);
+	    return;
+    }
+}
---
+++ Number Complement/src/Solution.java
@@ -0,0 +1,72 @@
+/*
+    Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary
+    representation.
+
+    Example 1:
+
+    Input: 5
+    Output: 2
+    Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010.
+    So you need to output 2.
+
+    Example 2:
+
+    Input: 1
+    Output: 0
+    Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0.
+    So you need to output 0.
+
+
+    Note:
+    The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+    You could assume no leading zero bit in the integer’s binary representation.
+    This question is the same as 1009: https://leetcode.com/problems/complement-of-base-10-integer/
+ */
+public class Solution {
+    /**
+     * My solution is to use a 32 bit array to remember the converting state.
+     *
+     * Move num to right (>>) i times (for bit on i), MOD with 2 (to extract the last digit), then XOR with 1 to covert
+     * until num is zero.
+     *
+     * Reverse, if the bit is 1, move 1 left (<<) i times and bitwise OR (|) with ans (initialized as 0).
+     *
+     * @param num
+     * @return
+     */
+    public int findComplement(int num) {
+        int[] bits = new int[32];
+        int d, ans = 0;
+        for(int i = 0; i < 32; i++){
+            d = num >> i;
+            if(d > 0) {
+                bits[i] = (d % 2) ^ 1;
+            } else {
+                break;
+            }
+        }
+
+        for(int i = 31; i >=0; i--) {
+            if(bits[i] == 1) {
+                ans = ans | 1 << i;
+            }
+        }
+
+        return ans;
+    }
+
+    /**
+     * A smarter solution is to convert num (for example 5: 101) to all 1s (111). Then use 111 - 101 to get 010 (2)
+     *
+     * @param num
+     * @return
+     */
+    public int findComplementLeetCode(int num) {
+        int n = 0;
+        while(n < num) {
+            n = (n << 1) | 1; //bitwise OR convert zero to 1 (n is initialized as all 0)
+        }
+        
+        return n - num;
+    }
+}
