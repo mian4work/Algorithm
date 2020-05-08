3a8b6f064f5bbde911e21eab3d300c29e1aff42f
Mian Zhang <mian4work@yahoo.com>
2020-05-07 18:03:08 -0700

    add comment

--- Cousins in Binary Tree/src/Solution.java
+++ Cousins in Binary Tree/src/Solution.java
@@ -1,3 +1,31 @@
+/*
+    In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+    Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+    We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+    Return true if and only if the nodes corresponding to the values x and y are cousins.
+       
+    Example 1:
+    Input: root = [1,2,3,4], x = 4, y = 3
+    Output: false
+
+    Example 2:
+    Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+    Output: true
+
+    Example 3:
+    Input: root = [1,2,3,null,4], x = 2, y = 3
+    Output: false
+
+
+    Note:
+
+    The number of nodes in the tree will be between 2 and 100.
+    Each node has a unique integer value from 1 to 100.
+ */
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -6,7 +34,7 @@ public class Solution {
      * My first try. Use a queue to save element in same depth. Use a pipe as a separator.
      * Check the children on the way down.
      *
-     * beat 100% but the logic looks ugly. Need find a better way. Should I not get into the children? 
+     * beat 100% but the logic looks ugly. Need find a better way. Should I not get into the children?
      *
      * @param root
      * @param x
