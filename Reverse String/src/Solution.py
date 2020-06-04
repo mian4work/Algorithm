3baa32fc12267ec630b7f1954ef6a23b010ff9ef
Mian Zhang <mian4work@yahoo.com>
2020-06-04 10:24:20 -0700

    Reverse String

---
+++ Reverse String/src/Solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start, end = 0, len(s) - 1
+        while start < end:
+            temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+            start += 1
+            end -= 1
+
+
+print(Solution().reverseString(["h", "e", "l", "l", "o"]))
